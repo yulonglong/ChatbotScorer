74d8e57cbba016bdfe3830e06781eb92f0bc5eaf
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-03-27 12:53:25 +0800

    Swapped command order

--- command/10.sh
+++ command/10.sh
@@ -35,6 +35,7 @@ echo "Running script on ${theano_flags_device} : ${gpu_name}"
 
 expt_num="010"
 dataset="IRIS"
+embedding="glove.6B"
 label_type="min"
 
 vocab_size="4000"
@@ -59,6 +60,7 @@ for rand in {1..5}
 do
     THEANO_FLAGS="device=${theano_flags_device},floatX=float32,mode=FAST_RUN" python main.py \
     -tr data/${dataset}.xml \
+    --emb embedding/${embedding}.${embedding_size}d.txt \
     -o expt${expt_num}${gpu_num}-${rand}-d${dataset}-v${vocab_size}-e${embedding_size}-t${model_type}-p${pooling_type}-c${cnn_dim}w${cnn_win}cl${cnn_layer}-r${rnn_type}${rnn_dim}rl${rnn_layer}-a${optimizer}-b${batch_size}-seed${rand}${gpu_num}78-${gpu_name} \
     -lt ${label_type} \
     -t ${model_type} -p ${pooling_type} \
--- command/8.sh
+++ command/8.sh
@@ -35,7 +35,6 @@ echo "Running script on ${theano_flags_device} : ${gpu_name}"
 
 expt_num="008"
 dataset="Joker"
-embedding="glove.6B"
 label_type="min"
 
 vocab_size="4000"
@@ -60,7 +59,6 @@ for rand in {1..5}
 do
     THEANO_FLAGS="device=${theano_flags_device},floatX=float32,mode=FAST_RUN" python main.py \
     -tr data/${dataset}.xml \
-    --emb embedding/${embedding}.${embedding_size}d.txt \
     -o expt${expt_num}${gpu_num}-${rand}-d${dataset}-v${vocab_size}-e${embedding_size}-t${model_type}-p${pooling_type}-c${cnn_dim}w${cnn_win}cl${cnn_layer}-r${rnn_type}${rnn_dim}rl${rnn_layer}-a${optimizer}-b${batch_size}-seed${rand}${gpu_num}78-${gpu_name} \
     -lt ${label_type} \
     -t ${model_type} -p ${pooling_type} \
