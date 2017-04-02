8242eb37d22f1edba0eb2f1511a676651dcedb9b
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-04-02 11:33:00 +0800

    Refactored command to run 3 datasets

--- command/28.sh
+++ command/28.sh
@@ -69,3 +69,37 @@ do
     --epochs ${num_epoch} -a ${optimizer} -e ${embedding_size} -v ${vocab_size} -do ${dropout} \
     -b ${batch_size} -be ${batch_eval_size} --seed ${rand}${gpu_num}78
 done
+
+expt_num="029"
+dataset="Joker"
+
+for rand in {1..5}
+do
+    THEANO_FLAGS="device=${theano_flags_device},floatX=float32,mode=FAST_RUN" python main.py \
+    -tr data/${dataset}.xml \
+    --emb embedding/${embedding}.${embedding_size}d.txt \
+    -o expt${expt_num}${gpu_num}-${rand}-d${dataset}-v${vocab_size}-e${embedding_size}-t${model_type}-p${pooling_type}-c${cnn_dim}w${cnn_win}cl${cnn_layer}-r${rnn_type}${rnn_dim}rl${rnn_layer}-a${optimizer}-b${batch_size}-seed${rand}${gpu_num}78-${gpu_name} \
+    -lt ${label_type} \
+    -t ${model_type} -p ${pooling_type} \
+    -cl ${cnn_layer} -c ${cnn_dim} -w ${cnn_win} \
+    -rl ${rnn_layer} -u ${rnn_type} -r ${rnn_dim} \
+    --epochs ${num_epoch} -a ${optimizer} -e ${embedding_size} -v ${vocab_size} -do ${dropout} \
+    -b ${batch_size} -be ${batch_eval_size} --seed ${rand}${gpu_num}78
+done
+
+expt_num="030"
+dataset="TickTock"
+
+for rand in {1..5}
+do
+    THEANO_FLAGS="device=${theano_flags_device},floatX=float32,mode=FAST_RUN" python main.py \
+    -tr data/${dataset}.xml \
+    --emb embedding/${embedding}.${embedding_size}d.txt \
+    -o expt${expt_num}${gpu_num}-${rand}-d${dataset}-v${vocab_size}-e${embedding_size}-t${model_type}-p${pooling_type}-c${cnn_dim}w${cnn_win}cl${cnn_layer}-r${rnn_type}${rnn_dim}rl${rnn_layer}-a${optimizer}-b${batch_size}-seed${rand}${gpu_num}78-${gpu_name} \
+    -lt ${label_type} \
+    -t ${model_type} -p ${pooling_type} \
+    -cl ${cnn_layer} -c ${cnn_dim} -w ${cnn_win} \
+    -rl ${rnn_layer} -u ${rnn_type} -r ${rnn_dim} \
+    --epochs ${num_epoch} -a ${optimizer} -e ${embedding_size} -v ${vocab_size} -do ${dropout} \
+    -b ${batch_size} -be ${batch_eval_size} --seed ${rand}${gpu_num}78
+done
