25a1b6787bc84849e7049002281f648fe6170764
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-04-10 01:16:07 +0800

    updated Summarizer

--- chatbotscorer/Summarize.java
+++ chatbotscorer/Summarize.java
@@ -30,11 +30,15 @@ public class Summarize {
 		
 		// Write Header
 		System.out.println("ExptName\tMaj-f1\tMajAcc\ttest1-f1\ttest1-Acc\ttest2-f1\ttest2-Acc\ttest3-f1\ttest3-Acc\ttest4-f1\ttest4-Acc\ttest5-f1\ttest5-Acc\ttest-ave-f1\ttest-ave-Acc");
+		System.out.println("ExptName\t\t\ttest1-PearsonAve\ttest1-PearsonTotal\ttest2-PearsonAve\ttest2-PearsonTotal\ttest3-PearsonAve\ttest3-PearsonTotal\ttest4-PearsonAve\ttest4-PearsonTotal\ttest5-PearsonAve\ttest5-PearsonTotal\ttest-ave-PearsonAve\ttest-ave-PearsonTotal");
+		
 		
 		String prevExptNum = "000";
 		int countExpt = 0;
 		Double totalTestF1 = 0.0;
 		Double totalTestAcc = 0.0;
+		Double totalTestAvePearson = 0.0;
+		Double totalTestTotalPearson = 0.0;
 		// Go through each of the folder names
 		for (String folderName: folderNames) {
 			String exptNum = folderName.substring(4,7);
@@ -44,6 +48,8 @@ public class Summarize {
 			
 			String majF1 = ""; String majAcc = "";
 			String testF1 = ""; String testAcc = "";
+			String testAvePearson = ""; String testAvePValue = "";
+			String testTotalPearson = ""; String testTotalPValue = "";
 			FileReader currentFile = new FileReader(s_testFolderName+"/"+folderName+"/log.txt");
 			BufferedReader br = new BufferedReader(currentFile);
 			String line;
@@ -62,19 +68,41 @@ public class Summarize {
 					testF1 = mTest.group(1);
 					testAcc = mTest.group(2);
 				}
+				String avePearsonRegex = "(?i)\\[AVG\\-TEST\\]   Correlation\\-coef: ([0-9\\.]+), p\\-value: ([0-9\\.]+)";
+				Pattern patternAvePearsonRegex = Pattern.compile(avePearsonRegex, Pattern.CASE_INSENSITIVE);
+				Matcher mAvePearson = patternAvePearsonRegex.matcher(line);
+				while (mAvePearson.find()) {
+					testAvePearson = mAvePearson.group(1);
+					testAvePValue = mAvePearson.group(2);
+				}
+				String totalPearsonRegex = "(?i)\\[TOTAL\\-TEST\\] Correlation\\-coef: ([0-9\\.]+), p\\-value: ([0-9\\.]+)";
+				Pattern patternTotalPearsonRegex = Pattern.compile(totalPearsonRegex, Pattern.CASE_INSENSITIVE);
+				Matcher mTotalPearson = patternTotalPearsonRegex.matcher(line);
+				while (mTotalPearson.find()) {
+					
+					testTotalPearson = mTotalPearson.group(1);
+					testTotalPValue = mTotalPearson.group(2);
+				}
 			}
 			if (!prevExptNum.equals(exptNum)) {
-				if (countExpt > 0)
+				if (countExpt > 0 && totalTestF1 > 0.0 && totalTestAcc > 0.0)
 					System.out.println( String.format( "%.3f", totalTestF1/(double)countExpt) + "\t" + String.format( "%.3f", totalTestAcc/(double)countExpt));
+				if (countExpt > 0 && totalTestAvePearson > 0.0 && totalTestTotalPearson > 0.0)
+					System.out.println( String.format( "%.3f", totalTestAvePearson/(double)countExpt) + "\t" + String.format( "%.3f", totalTestTotalPearson/(double)countExpt));
 				
 				System.out.print(folderName + "\t");
 				if (majF1.length() > 0 && majAcc.length() > 0) {
 					System.out.print(majF1 + "\t" + majAcc + "\t");
 				}
+				else {
+					System.out.print("\t\t");
+				}
 				prevExptNum = exptNum;
 				countExpt = 0;
 				totalTestF1 = 0.0;
 				totalTestAcc = 0.0;
+				totalTestAvePearson = 0.0;
+				totalTestTotalPearson = 0.0;
 			}
 			countExpt++;
 			if (testF1.length() > 0 && testAcc.length() > 0) {
@@ -82,8 +110,16 @@ public class Summarize {
 				totalTestF1 += Double.parseDouble(testF1);
 				totalTestAcc += Double.parseDouble(testAcc);
 			}
+			if (testAvePearson.length() > 0 && testTotalPearson.length() > 0) {
+				System.out.print(testAvePearson + "\t" + testTotalPearson + "\t");
+				totalTestAvePearson += Double.parseDouble(testAvePearson);
+				totalTestTotalPearson += Double.parseDouble(testTotalPearson);
+			}
 		}
-		System.out.println( String.format( "%.3f", totalTestF1/(double)countExpt) + "\t" + String.format( "%.3f", totalTestAcc/(double)countExpt));
+		if (countExpt > 0 && totalTestF1 > 0.0 && totalTestAcc > 0.0)
+			System.out.println( String.format( "%.3f", totalTestF1/(double)countExpt) + "\t" + String.format( "%.3f", totalTestAcc/(double)countExpt));
+		if (countExpt > 0 && totalTestAvePearson > 0.0 && totalTestTotalPearson > 0.0)
+			System.out.println( String.format( "%.3f", totalTestAvePearson/(double)countExpt) + "\t" + String.format( "%.3f", totalTestTotalPearson/(double)countExpt));
 	}
 
 	public static void showErrorMessage() {
