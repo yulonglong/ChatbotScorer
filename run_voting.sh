4c042768ba132efc7d6eb7c754958c9aa6dcc1fe
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-04-12 21:48:46 +0800

    Added run voting shell

---
+++ run_voting.sh
@@ -0,0 +1,5 @@
+python voting.py \
+-maxp expt0191-1-dIRISltmax-v4000-e100-tcnn-pmeanot-c300w3cl1-rlstm300rl2-armsprop-b16-seed1178-K80/preds/ \
+-minp expt0222-1-dIRISltmin-v4000-e100-tcnn-pmeanot-c300w3cl1-rlstm300rl2-armsprop-b16-seed1278-K80/preds/ \
+-ref expt1093-1-dIRISltmean-v4000-e100-tcnn-pmeanot-c300w3cl1-rlstm300rl2-armsprop-b16-seed1378-K80/preds/ \
+-o voting \
